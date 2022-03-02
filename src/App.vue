a2492b611738c385defb4999eac2e5fe906f925f
Bryan P <99640919+shurealis@users.noreply.github.com>
2022-03-02 12:39:56 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <div class="container mx-auto">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
